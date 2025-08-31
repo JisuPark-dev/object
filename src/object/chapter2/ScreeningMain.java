49a9dbd439b7f07e1119ff6790fb32392675a94f
sudong <dodlal123tkd@gmail.com>
2025-08-31 19:56:36 +0900

    책임 중심 설계, 객체지향 설계의 정수 = 캡슐화

--- src/object/chapter2/ScreeningMain.java
+++ src/object/chapter2/ScreeningMain.java
@@ -15,7 +15,8 @@ public class ScreeningMain {
     Movie avatar = new Movie("아바타",
         Duration.ofMinutes(120),
         Money.wons(10000),
-        new AmountDiscountPolicy(Money.wons(800),
+        new AmountDiscountPolicy(
+        Money.wons(800),
         new SequenceCondition(1),
         new SequenceCondition(10),
         new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59)),
